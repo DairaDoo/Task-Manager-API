dce7eaa38c611f448b6ad06e51337f287103d7b1
Dairan S. <dairandemora.work@gmail.com>
2025-01-28 18:40:17 -0400

    id de modelo de datos actualizado en el post para que sea autoincremental. Esto se hizo utilizando el ORM Entity Framework Core.

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TaskManagerAPI.Models;
 
 public class TaskModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // creado para que el id, se autoincremente.
     public int Id { get; set; } // primary key
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
